34b5597028e5cf0a431c26a9f102d8f69f5cfbcb
Szymon <szymon.wiercigroch@gmail.com>
2020-12-10 01:09:41 +0100

    fixing dumb misteakes

--- backend/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Room
-
-
-class RoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Room
-        fields = ('id', 'code', 'host', 'player_1', 'player_2', 'player_3', 'number_of_players')
